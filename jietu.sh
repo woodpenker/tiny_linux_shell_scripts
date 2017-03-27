111de91f44e7e8a6c4ed0817b4bb3897a7339949
woodpenker <woodpenker@163.com>
2017-03-27 11:10:14 +0800

    fix:jietu.sh

--- jietu.sh
+++ jietu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #这是截图脚本 wudy 2016-05-29
+#截图存放位置:修改此处即可,默认~/
+picloc="~/"
+
 export DISPLAY=:0.0
-if [ -d /media/d/资料文档/截图/ ] && [ -w /media/d/资料文档/截图/ ];then  
-gnome-screenshot -a -f /media/d/资料文档/截图/$(date +%Y%m%d_%H%M%S).png
-notify-send --expire-time=200 "截图已经保存到/media/d/资料文档/截图/"
+if [ -d $picloc ] && [ -w $picloc ];then  
+gnome-screenshot -a -f $picloc/$(date +%Y%m%d_%H%M%S).png
+notify-send --expire-time=200 "截图已经保存到$picloc"
 else 
-notify-send --expire-time=200 "截图出现问题，保存位置不存在或者目录不可被写入！"
+notify-send --expire-time=200 "截图出现问题，保存位置$picloc不存在或者目录不可被写入！"
 fi
