b0631342c51be8f6ef7bee55851ddea29dbc7a0a
woodpenker <woodpenker@163.com>
2017-08-15 20:22:10 +0800

    update wc-all.sh to exclude ELF files.

--- wc-all.sh
+++ wc-all.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+############
+# Usage:
+#" ./wc-all.sh -l1 /home"
+#" ./wc-all.sh -l2 /home"
+############
 function loop1()
 {
     for i in $(ls  .)
@@ -9,12 +14,14 @@ function loop1()
             loop1 $i
             cd ..
         elif [ -f $i ];then
-            wc $i
-            lcount=$(($lcount + $(wc -l $i |cut -d ' ' -f1)))
-            wcount=$(($wcount + $(wc -w $i |cut -d ' ' -f1)))
-            ccount=$(($ccount + $(wc -c $i |cut -d ' ' -f1)))
-            mcount=$(($mcount + $(wc -m $i |cut -d ' ' -f1)))
-
+            name=$(file "$i"| grep "ELF")
+            if [ "$name" = "" ];then
+               wc $i
+              lcount=$(($lcount + $(wc -l $i |cut -d ' ' -f1)))
+              wcount=$(($wcount + $(wc -w $i |cut -d ' ' -f1)))
+              ccount=$(($ccount + $(wc -c $i |cut -d ' ' -f1)))
+              mcount=$(($mcount + $(wc -m $i |cut -d ' ' -f1)))
+              fi
         fi
     done
 }
@@ -25,22 +32,29 @@ function loop2()
    files=$(find $1)
    for i in $files; do
     if [ ! -d $i ];then
+      name=$(file "$i"| grep "ELF")
+      if [ "$name" = "" ];then
        wc $i
        lcount=$(($lcount + $(wc -l $i |cut -d ' ' -f1)))
         wcount=$(($wcount + $(wc -w $i |cut -d ' ' -f1)))
         ccount=$(($ccount + $(wc -c $i |cut -d ' ' -f1)))
         mcount=$(($mcount + $(wc -m $i |cut -d ' ' -f1)))
+      fi
    fi
    done
 }
-if [ "$2" = "1" ];then
+if [ "$1" = "-l1" ];then
+    shift
     nowpwd=$(pwd)
     cd $1
     loop1
     echo -e "Total: l:$lcount w:$wcount c:$ccount m:$mcount"
     cd $nowpwd
     unset nowpwd
-else
+elif [ "$1" = "-l2" ];then
+      shift
      loop2 $1
       echo -e "Total: l:$lcount w:$wcount c:$ccount m:$mcount"
+else
+    echo -e "Usage: \n\" ./wc-all.sh -l1 /home\"\n\" ./wc-all.sh -l2 /home\" "
 fi
